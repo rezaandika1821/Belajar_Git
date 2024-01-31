7b60f06eeff9c1ed0f8aaff799dc5427fb997ed6
rezaaandika1821 <rezaandika1821@gmail.com>
2024-01-31 14:23:03 +0700

    input output

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var a int
+	b := 70
+	fmt.Println("Hello World")
+	fmt.Printf("ini umur %d kakek saya " ,b)
+	fmt.Println("Hello World")
+	fmt.Scan(&a)
+	fmt.Println("ini umur saya ", a)
+}
