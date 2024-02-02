97ce791d6bcfcea1922229b731571f9f30d1705c
golangiticm <golangiticm@gmail.com>
2024-02-02 09:06:24 +0700

    repeat strings

--- main.go
+++ main.go
@@ -2,14 +2,21 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 func main() {
 	var a int
 	b := 70
 	fmt.Println("Hello World")
-	fmt.Printf("ini umur %d kakek saya " ,b)
+	fmt.Printf("ini umur %d kakek saya ", b)
 	fmt.Println("Hello World")
+	fmt.Print("Masukkan Umur Anda")
 	fmt.Scan(&a)
 	fmt.Println("ini umur saya ", a)
+
+	fmt.Println(strings.Repeat("=", 20))
+	fmt.Println()
+	fmt.Println(strings.Repeat("=", 20))
+
 }
