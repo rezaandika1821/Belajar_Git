2fda45fbfd19df5a12558d603e2035f0ddd094c6
golangiticm <golangiticm@gmail.com>
2024-02-02 09:16:56 +0700

    login sederhana

--- main.go
+++ main.go
@@ -8,15 +8,27 @@ import (
 func main() {
 	var a int
 	b := 70
-	fmt.Println("Hello World")
-	fmt.Printf("ini umur %d kakek saya ", b)
-	fmt.Println("Hello World")
-	fmt.Print("Masukkan Umur Anda")
-	fmt.Scan(&a)
-	fmt.Println("ini umur saya ", a)
 
-	fmt.Println(strings.Repeat("=", 20))
-	fmt.Println()
-	fmt.Println(strings.Repeat("=", 20))
+	var sandi int
+	fmt.Print("Masukkan Sandi Anda : ")
+	fmt.Scanln(&sandi)
+	if sandi != 230104 {
+		fmt.Println("Sandi Salah")
+		fmt.Println("Anda di Blokir")
+		goto a
+	} else if sandi == 230104 {
 
+		fmt.Println("Hello World")
+		fmt.Printf("ini umur %d kakek saya ", b)
+		fmt.Println("Hello World")
+		fmt.Print("Masukkan Umur Anda")
+		fmt.Scan(&a)
+		fmt.Println("ini umur saya ", a)
+
+		fmt.Println(strings.Repeat("=", 20))
+		fmt.Println()
+		fmt.Println(strings.Repeat("=", 20))
+
+	}
+a:
 }
